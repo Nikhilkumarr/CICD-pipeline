ee54cd69936f99117d63fc4c62926848653bc333
Nikhilkumarr <bnikhil.1998@gmail.com>
2024-03-13 05:45:16 +0530

    changed the variables to new access keys

--- variable.tf
+++ variable.tf
@@ -1,9 +1,9 @@
 variable "access_key" {
-  default = "AKIAXETNQAOYPLM5BJZY"
+  default = "AKIA3FLDW7MTAG7EPE42"
   }
 variable "secret_key" {
-  default = "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"
+  default = "05ca0+0ZLDM9CIhdBPIZlCZ20juTc340CGa+gBYT"
   }
 variable "region" {
-  default = "ap-south-1"
+  default = "us-east-1"
   }
